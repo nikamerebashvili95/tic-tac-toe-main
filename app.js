dbfe05ac4f096f62b13284637ffdb53b03554c2f
Nika Merebashvili <114658102+nikamerebashvili95@users.noreply.github.com>
2023-03-20 01:55:29 -0700

    work with game buttons click functions and change some logic of game

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 const choiceButtons = document.querySelectorAll(".btn-box");
+const playButtons = document.querySelectorAll(".play-btn");
 const home = document.querySelector("#home");
 const board = document.querySelector("#board");
 let player1 = "x";
 let mode = "cpu";
+let turn = "x";
+let freeButtons = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+let xArray = [];
+let oArray = [];
+let winnerCombinations = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
 const activateChoice = (icon) => {
   if (icon === "x") {
     choiceButtons[0].classList.add("active");
@@ -15,9 +30,49 @@ const activateChoice = (icon) => {
   }
 };
 
+const onHoverEffects = () => {
+  for (let i = 0; i < freeButtons.length; i++) {
+    const playButtonIndex = freeButtons[i];
+    if (turn === "x") {
+      playButtons[playButtonIndex].classList.add("xHover");
+      playButtons[playButtonIndex].classList.remove("oHover");
+    } else {
+      playButtons[playButtonIndex].classList.add("oHover");
+      playButtons[playButtonIndex].classList.remove("xHover");
+    }
+  }
+};
+
+const createClickedFunctions = () => {
+  for (let i = 0; i < playButtons.length; i++) {
+    playButtons[i].onclick = (e) => {
+      e.target.classList.remove("xHover");
+      e.target.classList.remove("oHover");
+      const spliceIndex = freeButtons.indexOf(i);
+      freeButtons.splice(spliceIndex, 1);
+
+      const icon = document.createElement("img");
+      icon.classList.add("play-icon");
+      if (turn === "x") {
+        icon.src = "./assets/icon-x.svg";
+        e.target.append(icon);
+        turn = "o";
+      } else {
+        icon.src = "./assets/icon-o.svg";
+        e.target.append(icon);
+        turn = "x";
+      }
+      onHoverEffects();
+      e.target.onclick = null;
+    };
+  }
+};
+
 const startGame = (modeParam) => {
   home.style.display = "none";
   board.style.display = "flex";
   document.body.style.alignItems = "flex-start";
   mode = modeParam;
+  onHoverEffects();
+  createClickedFunctions();
 };
